02e992b7e56cf9a54aebc9ba090741e2d678b6c3
강보원 <gangbowon@gangbowon-ui-MacBookAir.local>
2025-01-17 04:29:19 +0900

    update

--- example/example.py
+++ example/example.py
@@ -9,8 +9,8 @@ headers = {
 
 data = {
     'input'        : '욕설이 포함될수 있는 메시지',
-    'replace-front': '<',
-    'replace-end'  : '>'
+    'replace-front': '감지된 욕설 앞부분에 넣을 메시지 (옵션)',
+    'replace-end'  : '감지된 욕설 뒷부분에 넣을 메시지 (옵션)'
 }
 
 response = requests.post(url, headers=headers, json=data)
